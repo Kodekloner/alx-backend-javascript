924c17b008c456827543e150a431d34537fee095
Kodekloner <billgateokoye@gmail.com>
2024-08-26 13:09:04 +0100

    Updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stderr.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('data', data => {
+  console.log(`You typed : ${data.toString().trim()}`);
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+  console.log('This important software is now closing')
+  process.exit(0)
+})
